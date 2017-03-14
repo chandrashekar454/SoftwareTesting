dffbe811bf319c9c31eb5cd5947c3dd908ec313b
ChandrashekarChary <chandrashekar445544@gmail.com>
2017-03-14 06:31:04 -0400

    final

---
+++ src/Basicss/Xpaths.java
@@ -0,0 +1,23 @@
+//Sending text to searchBox//////////////////////////////////////////
+	  driverMethod.SendKeyMethod("/html/body/div[5]/div[1]/div[1]/form/input[1]","iphone 7");
+	  //driverMethod.SendKeyMethod(".//*[@id='vgn_q']","iphone 7");
+	 
+	  // driver.findElement(By.xpath(".//*[@id='vgn_q']")).sendKeys("iphone 7");
+	  
+	  
+//Search Button////////////////////////////////////////////////////////
+	  
+	  driverMethod.elementClick("/html/body/div[5]/div[1]/div[1]/form/button[2]/span[1]");
+	  //driverMethod.elementClick(".//*[@id='vgn_search_btn']");
+	  //driver.findElement(By.xpath(".//*[@id='vgn_search_btn']")).click();
+
+//Selecting Phone//////////////////////
+	  driverMethod.elementClick("/html/body/div[5]/section/div[3]/section/div[3]/div[1]/div[1]/div[3]/div[1]/a/span");
+	  //driverMethod.elementClick(".//*[@id='grid']/div[1]/div[3]/div[1]/a");
+	 
+	  // driver.findElement(By.xpath(".//*[@id='grid']/div[1]/div[3]/div[1]/a")).click();
+//Next Step/////////////////////////////
+	  driverMethod.elementClick("/html/body/main/section/div[2]/section[1]/section[2]/section[2]/section[1]/section[10]/button[1]");
+	  //driverMethod.elementClick(".//*[@id='myBtn']");
+	 // driver.findElement(By.xpath(".//*[@id='myBtn']")).click();
+	  
